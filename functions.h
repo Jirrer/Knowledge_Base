6a4031a1f34df888af7532d532d3aabeab537d81
Jirrer <jrirrer@gmail.com>
2026-03-01 15:33:50 -0500

    building out menu methods

--- functions.h
+++ functions.h
@@ -2,10 +2,13 @@
 
 using namespace std;
 
+enum Choice{};
+
 #ifndef MATH_UTILS_H 
 #define MATH_UTILS_H
 
 void showSearchResults(string searchOutput);
 string searchKnowledgeBase(string searchInput);
+Choice getChoice();
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
-#include "functions.h"
 #include <iostream>
+#include <conio.h>
+#include <vector>
 
 using namespace std;
 
@@ -8,30 +9,107 @@ enum Choice {
     search_base,
 };
 
+void clearTerminal();
+Choice getChoice(string choiceInput);
+void searchKnowledgeBase(string searchInput);
+void showSelectedOutput(string resultsID);
+
+void clearTerminal() {
+    // To-Do: accept linux as an option
+
+    system("cls");
+}
+
 int main() {
     enum Choice choice;
+    string userInput;
     
     while (choice != exit_program) {
-        cout << "Ran loop";
+        cout << ":";
 
-        choice = exit_program;
+        getline(cin, userInput);
+
+        choice = getChoice(userInput);
+
+        clearTerminal();
 
         switch (choice) {
             case exit_program: exit;
-            case search_base: showSearchResults(searchKnowledgeBase("")); 
+            case search_base: searchKnowledgeBase(userInput); 
         }
     }
 
     return 0;
 }
 
-void showSearchResults(string searchOutput) {
-    cout << searchOutput;
+Choice getChoice(string choiceInput) {
+    if (choiceInput == "leave") {
+        return Choice::exit_program;
+
+    } else {
+        return Choice::search_base;
+    }
 }
 
-string searchKnowledgeBase(string searchInput) {
+void searchKnowledgeBase(string searchInput) {
+    // To-Do: fuzzy search knowledge base and show
+    // results (might move to another file)
+    vector<string> searchResults = {
+        "copying working directory in windows",
+        "traversing a binary tree",
+        "copying working directory in windows"
+    };
 
+    clearTerminal();
 
-    return "test";
+    cout << "*** Search Results ***" << endl;
+
+    for (string result : searchResults) {
+        cout << result << endl;
+    }
+
+    cout << "\033[2;1H";
+
+    int lineIndex = 0;
+    while (true) {
+        int keySelection = _getch();
+
+        if (keySelection == 13) { break; }
+
+        // To-Do: put cursor at the end of the text
+        
+        if (keySelection == 0 || keySelection == 224) {
+            int arrowDirection = _getch();
+            
+            if (arrowDirection == 72 and lineIndex > 0) { 
+                lineIndex--; 
+                cout << "\033[1A";
+            }
+            else if (arrowDirection == 80 and lineIndex < searchResults.size() - 1) { 
+                lineIndex++; 
+                cout << "\033[1B";
+            } 
+        }
+    }
+
+    showSelectedOutput(searchResults.at(lineIndex));
 }
+
+
+
+// Move cursor up 1 line
+// std::cout << "\033[1A";
+// Move cursor down 1 line
+// std::cout << "\033[1B";
+// Move cursor right 1 position
+// std::cout << "\033[1C";
+
+
+void showSelectedOutput(string resultsID) {
+    clearTerminal();
+
+    cout << resultsID << endl;
+    cout << "example data" << endl;
+}
+
 
